5fc6f77d790bdfc7939f7bff05dcaaf6902e19b9
Albie04 <121083782+Albie04@users.noreply.github.com>
2025-03-12 21:45:56 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
-let personname = prompt('как тебя зовут?');
-localStorage.setItem('name', personname);
+function entername() {
+	let personname = prompt('как тебя зовут?');
+	localStorage.setItem('name', personname);
+}
+function showname() {
+	alert(localStorage.getItem('name'));
+}
